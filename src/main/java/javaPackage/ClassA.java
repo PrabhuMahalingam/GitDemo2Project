ad1648263ac2ae25ea1a76b90fb0bdab47095c6f
prabhu <mahalingam.prabu@gmail.com>
2021-04-06 13:28:33 +0530

    First Commit

---
+++ src/main/java/javaPackage/ClassA.java
@@ -0,0 +1,12 @@
+package javaPackage;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Class A.......");
+
+	}
+
+}
